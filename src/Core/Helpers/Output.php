66bfffdd282ba1bd125467a03998ae6831d17d5c
Jairo Santos <jairojeffersont@gmail.com>
2025-06-26 15:42:48 -0300

    v1

--- src/Core/Helpers/Output.php
+++ src/Core/Helpers/Output.php
@@ -4,16 +4,14 @@ namespace GoFrame\Core\Helpers;
 
 class Output {
     public function buildOutput(array $array) {
-        // Verifica se existe um código HTTP e o define
+
         if (isset($array['status_code'])) {
             http_response_code($array['status_code']);
            
         }
 
-        // Define o header de conteúdo como JSON
         header('Content-Type: application/json; charset=utf-8');
 
-        // Envia o array como JSON
         echo json_encode($array, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
         exit;
     }
